46eab9f5a349098b2e026ba9cd8219c806c3eb86
Nathaniel Titterton <nate@berkeley.edu>
2013-11-19 09:52:12 -0800

    first commit, nothing really yet

---
+++ plugin.js
@@ -0,0 +1,20 @@
+///  draft Snap! plugin controller
+
+// takes a string as namespace
+function Plugin(id) {
+	this.id = id;
+
+
+}
+
+Plugin.prototype.idAlreadyInstalled() {
+	if 
+}
+
+Plugin.prototype.install = function() {
+	if (id == "") {
+		throw "Plugin misconfigured: no plugin name";
+	} else if ()
+	
+}
+
